aa887b2de8d8f4a23d56bb70b84872b983c3ce50
harry <hkiang01@yahoo.com>
2015-04-09 22:58:26 -0500

    some python parsing, not complete, but just a single digit

---
+++ python/Digit.py
@@ -0,0 +1,11 @@
+#Class Digit
+class Digit(object):
+    #define class to store the digit
+    def ___init___(self): #default constructor
+        self.number = []
+    def printNumber(self): #digit is implicit argument
+        print self.number
+    def getNumber(self):
+        return self.number
+    
+    
---
+++ python/main.py
@@ -0,0 +1,21 @@
+from Digit import *
+
+f = open("testimages", "r") #opens trainingimages
+
+masterList = [] # a list of Digits
+
+for i in range (0, 5000):    
+    currList = [] #the array of lines for each digit
+    curr_digit = Digit() 
+    for i in range(0, 28):
+        currline = f.readline()
+        currList.append(currline)
+    curr_digit.number = currList
+    masterList.append(curr_digit)
+
+for i in range(0,1):
+    masterList[i].printNumber()
+
+print ("end of main")
+
+f.close()
