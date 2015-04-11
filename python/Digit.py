e29306c453612da3c6ee84cb3ebe70668a098f83
harry <hkiang01@yahoo.com>
2015-04-10 20:34:23 -0500

    calcSimilarityHeuristic works for first 1000 entries, and then out of range error for some readon

--- python/Digit.py
+++ python/Digit.py
@@ -1,21 +1,66 @@
+import numpy as np #importing numpy (not used at time of writing)
+import scipy as sp #importing scipy (not used at time of writing)
+
+
+# GLOBAL 
+NUM_ROWS = 28
+NUM_COLS = 29
 
 #Class Digit
 class Digit(object):
+
     #define class to store the digit
     def ___init___(self): #default constructor
         self.number = []
+        self.features = []
+    def processFeatures(self):
+    	ret_list = []
+    	for s in self.number: #s is a string
+    		curr_line = []
+    		for c in s:
+    			if(c=='#' or c=='*'):
+    				curr_line.append(1)
+    			else:
+    				curr_line.append(0)
+    		ret_list.append(curr_line)
+    	self.features = ret_list
     # reference: http://www.kosbie.net/cmu/fall-11/15-112/handouts/notes-2d-lists.html
     def printNumber(self): #digit is implicit argument
         if (self.number==[]): # null check for empty list
         	print [] # prevents crashing on accessing number[0]
         	return
-        rows = len(self.number)
-        cols = len(self.number[0])
-        #but all rows have 28 cols, so we don't really need maxItemLength()
-        for row in xrange(rows): #xrange returns xrange object
+        for row in xrange(NUM_ROWS): #xrange returns xrange object
         # xrange, see: https://wiki.python.org/moin/ForLoop
         # xrange is more efficient with memory than range
         	curr_line = ""
-        	for col in xrange(cols): 
+        	for col in xrange(NUM_COLS): 
         		curr_line += self.number[row][col]
         	print curr_line,
+    def printFeatures(self):
+    	for row in xrange(NUM_ROWS):
+    		curr_line = ""
+    		for col in xrange(NUM_COLS):
+    			curr_line += (str(self.features[row][col]))
+    		print curr_line
+    def calcSimilarityHeuristic(self, a_digit):
+    	heuristic = 0
+    	for row in xrange(0, NUM_ROWS):
+    		for col in xrange(0, NUM_COLS):
+    			if(self.features[row][col] == a_digit.features[row][col] and a_digit.features[row][col]==1):
+    				heuristic += 1
+    	return heuristic
+
+
+
+### NOTES ###
+
+# DIVISION
+# / #float
+# // #floor
+
+# FOR LOOPS
+# for row in xrange(0, NUM_ROWS)
+# same as
+# for row in xrange(NUM_ROWS)
+# lower bound inclusive, assumed 0
+# upper bound exclusive, must be defined
--- python/main.py
+++ python/main.py
@@ -1,20 +1,35 @@
+import sys, getopt
 from Digit import *
+
+debug_small = False
+debug_large = True
 
 f = open("testimages", "r") #opens trainingimages
 
 masterList = [] # a list of Digits
 
-for i in range (0, 5000):    
+for i in xrange (0, 5000):    
     currList = [] #the array of lines for each digit
     curr_digit = Digit() 
-    for i in range(0, 28):
+    for i in xrange(0, 28):
         currline = f.readline()
         currList.append(currline)
     curr_digit.number = currList
+    curr_digit.processFeatures()
     masterList.append(curr_digit)
 
-for i in range(0,1):
-    masterList[i].printNumber()
+if(debug_small):
+	for digit in range(0,1):
+	    masterList[digit].printNumber()
+	    masterList[digit].printFeatures()
+
+if(debug_large):
+	counter = 0
+	for digit in range(0, (len(masterList))):
+		print masterList[counter].calcSimilarityHeuristic(masterList[counter+1])
+		counter+=1
+		print "Counter", counter
 
 
+print "\nend of main"
 f.close()
