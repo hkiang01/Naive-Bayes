73ea850d6d8c16ab8cef8054fcb7cb2ad205572f
harry <hkiang01@yahoo.com>
2015-04-18 20:39:20 -0500

    parsed test 8cat

--- python/part2/Part2.py
+++ python/part2/Part2.py
@@ -24,6 +24,9 @@ class Part2(object):
 	masterTraining8catDictionaryList = []
 	training8catLabels = []
 
+	masterTest8catDictionaryList = []
+	test8catLabels = []
+
 	master8catDictList = [{},{},{},{},{},{},{},{}] # list of dictionaries for each class (catetory)
 	num8catWords = [0,0,0,0,0,0,0,0] # counters
 	priors8cat = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
@@ -281,11 +284,48 @@ class Part2(object):
 				self.master8catDictList[category][word] = curr_value
 				print word, self.master8catDictList[category].get(word)
 			#break #first dictionary
-			print self.num8catWords[category]
 			category += 1
 
+	def parseTest8cat(self, filename):
+				#each line is a document
+		#each document: [label] [word1]:[count1] [word2]:[count2] ... [wordn]:[countn]
+		#labels for email: 0 is normal, 1 is spam
+		#labels for 8category: 0 is sci.space; 1 is comp.sys.ibm.pc.hardware;
+		#2 is rec.sport.baseball; 3 is comp.windows.x; 4 is talk.politics.misc
+		#5 is misc.forsale; 6 is rec.sport.hockey; 7 is comp.graphics
 
-	def __init__(self, filename_email_training, filename_8cat_training, filename_email_test):
+		# create dictionaries consisting of all UNIQUE words occurring in the training documents
+
+		f = open(filename, "r")
+		content = f.readlines()
+		f.close()
+		for line in content: #for each email document
+			curr_dict = {}
+			# rsplit(sep[, maxsplit]]) Return a list of the words in the string, using sep as the delimiter string
+			elements = line.rsplit(' ')
+			curr_label = int(elements[0])
+			#print "Counter:", i, "Label:",curr_label
+			self.test8catLabels.append(curr_label)
+
+			#skip the first element
+			#http://stackoverflow.com/questions/10079216/skip-first-entry-in-for-loop-in-python
+			iterElements = iter(elements) #skip the first element
+			next(iterElements) #the first element contains the label
+			for elem in iterElements:
+				#word:count
+			    #print elem
+			    temp = elem.rsplit(':')
+			    key = temp[0]
+			    value = int(temp[1])
+			    #print "Key:", key, "Value:", value
+			    curr_dict[key] = value #add entry into dictionary
+			self.masterTest8catDictionaryList.append(curr_dict)
+
+	def printTest8catLabels(self):
+		for label in self.test8catLabels:
+			print label
+
+	def __init__(self, filename_email_training, filename_8cat_training, filename_email_test, filename_8cat_test):
 
 		# #training email
 		# self.parseTrainingEmails(filename_email_training)
@@ -312,4 +352,6 @@ class Part2(object):
 		self.print8catNumWordsAll()
 		self.calc8catPriors()
 		self.calc8catProbabilityTables()
+		self.parseTest8cat(filename_8cat_test)
+		self.printTest8catLabels()
 
